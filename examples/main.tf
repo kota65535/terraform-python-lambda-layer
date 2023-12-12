85232871a59ec34ef8c4209a52e12b70b21edb7a
Tomohiko Ozawa <kota65535@gmail.com>
2023-12-12 11:58:43 +0900

    Change requirements_file to requirements_path (#3)

--- examples/main.tf
+++ examples/main.tf
@@ -2,6 +2,6 @@ module "lambda_layer" {
   source            = "../"
   name              = "test"
   python_version    = "3.11.0"
-  requirements_file = "requirements.txt"
+  requirements_path = "requirements.txt"
   output_path       = "${path.root}/test-layer.zip"
 }
